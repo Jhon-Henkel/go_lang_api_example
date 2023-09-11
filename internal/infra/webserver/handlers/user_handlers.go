799865e45af4b5ad6ee21eb7b2268d959a331bdc
Jhonatan Henkel <jhowrf@gmail.com>
2023-09-11 07:18:00 -0300

    Copiando a pasta do outro projeto para esse

---
+++ internal/infra/webserver/handlers/user_handlers.go
@@ -0,0 +1,41 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Jhon-Henkel/go_lang_api_example/tree/main/internal/dto"
+	"github.com/Jhon-Henkel/go_lang_api_example/tree/main/internal/entity"
+	"github.com/Jhon-Henkel/go_lang_api_example/tree/main/internal/infra/database"
+	"github.com/go-chi/jwtauth"
+)
+
+type UserHandler struct {
+	UserDB       database.UserInterface
+	Jwt          *jwtauth.JWTAuth
+	JwtExpiresIn int
+}
+
+func NewUserHandler(db database.UserInterface) *UserHandler {
+	return &UserHandler{UserDB: db}
+}
+
+func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
+	var user dto.CreateUserInput
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	userToInsert, err := entity.NewUser(user.Name, user.Email, user.Password)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = h.UserDB.Create(userToInsert)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
